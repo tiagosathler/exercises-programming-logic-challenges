39f0d1000cb4e918abef967d1a5e3f6a5d11b115
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-08-04 10:49:37 -0300

    ✨ feat: resolve os desafios de 'Destructuring'

--- Desafios/Destructuring e HOFs/desafio-hofs-04.js
+++ Desafios/Destructuring e HOFs/desafio-hofs-04.js
@@ -1,9 +1,13 @@
-const data = require('./data');
+const data = require("./data");
 
 // 4 - Filtre e retorne um array apenas com o nome das bandas que são dos Estados Unidos
 
 const usaFilter = () => {
-
-}
+  return data.bands
+    .filter(
+      ({ country }) => country === "United States" || country === "England"
+    )
+    .map(({ bandName }) => bandName);
+};
 
 module.exports = usaFilter;
--- Desafios/Destructuring e HOFs/desafio-hofs-06.js
+++ Desafios/Destructuring e HOFs/desafio-hofs-06.js
@@ -1,9 +1,17 @@
-const data = require('./data');
+const data = require("./data");
 
 // 6 - Filtre e retorne um array com todos os álbuns que possuem nota igual a 100
 
 const highestRatingFilter = () => {
-
-}
+  return data.bands.flatMap(({ bestAlbuns }) =>
+    bestAlbuns.flatMap((album) => (album.rate == 100 ? album : []))
+  );
+  // return data.bands
+  // .reduce((acc, { bestAlbuns }) => {
+  //   const album = bestAlbuns
+  //     .filter(({ rate }) => rate === 100);
+  //   return [...acc, ...album];
+  // }, []);
+};
 
 module.exports = highestRatingFilter;
